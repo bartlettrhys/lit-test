ed72fe3b4dd184e8e6b696bd5d002ee99ea6d734
Rhys Bartlett <rhysbartlett@Rhyss-MacBook-Air.local>
2023-07-24 15:15:01 +0100

    .

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,11 @@
-/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
+// rollup.config.js
 import summary from 'rollup-plugin-summary';
 import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript'; // Add this line
 
 export default {
-  input: 'src/my-element.ts',
+  input: 'src/my-element.ts', // Update the entry file to .ts
   output: {
     file: 'dist/my-element.bundled.js',
     format: 'esm',
@@ -21,8 +16,8 @@ export default {
     }
   },
   plugins: [
-    replace({'Reflect.decorate': 'undefined'}),
     resolve(),
+    typescript(), // Add this line
     terser({
       ecma: 2017,
       module: true,
